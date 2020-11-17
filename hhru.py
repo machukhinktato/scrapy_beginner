5a0048b5bab1151845ba876ec7208a9146b550bc
machukhinktato <tarabrinmv@gmail.com>
2020-11-17 17:47:00 +0300

    in progress

--- hhru.py
+++ hhru.py
@@ -1,10 +1,23 @@
 import scrapy
+from scrapy.http import HtmlResponse
+from jobparser.items import JobparserItem
 
 
 class HhruSpider(scrapy.Spider):
     name = 'hhru'
     allowed_domains = ['hh.ru']
-    start_urls = ['http://hh.ru/']
+    start_urls = ['https://izhevsk.hh.ru/search/vacancy?area=&fromSearchLine=true&st=searchVacancy&text=python']
 
-    def parse(self, response):
-        pass
+    def parse(self, response:HtmlResponse):
+        links = response.xpath("//a[@class='bloko-link HH-LinkModifier']/@href").extract()
+        next_page = response.xpath("//a[contains(@class,'HH-Pager-Controls-Next')]/@href").extract_first()
+        for link in links:
+            yield response.follow(link, callback=self.vacancy_parse)
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+
+    def vacancy_parse(self,response:HtmlResponse):
+        name = response.xpath("//h1/text()").extract_first()
+        salary = response.xpath("//p/span[@data-qa='bloko-header-2']/text()").extract()
+        yield JobparserItem(name=name, salary=salary)
