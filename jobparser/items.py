aeb814020ca51f2713913104a38ba0247dc23573
machukhinktato <tarabrinmv@gmail.com>
2020-11-18 15:56:06 +0300

    in progress

--- jobparser/items.py
+++ jobparser/items.py
@@ -1,12 +1,7 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class JobparserItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    _id = scrapy.Field()
+    name = scrapy.Field()
+    salary = scrapy.Field()
