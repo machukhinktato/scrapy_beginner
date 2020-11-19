699f7be7ea78deef6ff920fbdc13c40e6a255c4b
machukhinktato <tarabrinmv@gmail.com>
2020-11-19 16:08:38 +0300

    in progress

--- jobparser/spiders/hhru.py
+++ jobparser/spiders/hhru.py
@@ -6,21 +6,19 @@ from jobparser.items import JobparserItem
 class HhruSpider(scrapy.Spider):
     name = 'hhru'
     allowed_domains = ['hh.ru']
-    start_urls = ['https://hh.ru/search/vacancy?area=1&fromSearchLine=true&st=searchVacancy&text=Повар&from=suggest_post']
+    start_urls = [
+        'https://hh.ru/search/vacancy?area=1&fromSearchLine=true&st=searchVacancy&text=Повар&from=suggest_post']
 
-    def parse(self, response:HtmlResponse):
+    def parse(self, response: HtmlResponse):
         links = response.xpath("//a[@class='bloko-link HH-LinkModifier']/@href").extract()
-        # next_page = response.xpath("//a[contains(@class,'HH-Pager-Controls-Next')]/@href").extract_first()
+        next_page = response.xpath("//a[contains(@class,'HH-Pager-Controls-Next')]/@href").extract_first()
         for link in links:
             yield response.follow(link, callback=self.vacancy_parse)
-        # if next_page:
-        #     yield response.follow(next_page, callback=self.parse)
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
 
-
-    def vacancy_parse(self,response:HtmlResponse):
+    def vacancy_parse(self, response: HtmlResponse):
         name = response.xpath("//h1/text()").extract_first()
-        # salary = response.xpath("//p/span[@data-qa='bloko-header-2']/text()").extract()
-        # salary = response.xpath("//p/span[@data-qa='bloko-header-2']/text()").extract()
         min_salary = response.xpath("//p/span[@data-qa='bloko-header-2']/text()").extract()
         try:
             max_salary = [min_salary[3] if 'до ' in min_salary[2] else None]
@@ -30,9 +28,6 @@ class HhruSpider(scrapy.Spider):
             else:
                 max_salary = None
         min_salary = [min_salary[1] if 'от ' in min_salary else None]
-        # if len(salary) == 5:
-        #     min_salary = salary[1].replace('\xa0', '')
-        #     max_salary = salary[3].replace('\xa0', '')
         link = response._url
 
         print()
