5b288ef0c28810b73ad5f58babb687ba070b6789
machukhinktato <tarabrinmv@gmail.com>
2020-11-19 16:07:54 +0300

    in progress

--- jobparser/spiders/sjru.py
+++ jobparser/spiders/sjru.py
@@ -4,7 +4,7 @@ import scrapy
 class SjruSpider(scrapy.Spider):
     name = 'sjru'
     allowed_domains = ['superjob.ru']
-    start_urls = ['http://superjob.ru/']
+    start_urls = ['https://www.superjob.ru/vacancy/search/?keywords=pyrhon&geo%5Bt%5D%5B0%5D=4']
 
     def parse(self, response):
         pass
