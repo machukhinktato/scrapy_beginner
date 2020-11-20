75144dfb5ba3b4abbd2d6fc5667ceaa7b3c4db9f
machukhinktato <tarabrinmv@gmail.com>
2020-11-20 09:37:51 +0300

    in progress

--- jobparser/spiders/sjru.py
+++ jobparser/spiders/sjru.py
@@ -8,10 +8,10 @@ class SjruSpider(scrapy.Spider):
     name = 'sjru'
     allowed_domains = ['superjob.ru']
     # start_urls = ['https://www.superjob.ru/vacancy/search/?keywords=python&geo%5Bt%5D%5B0%5D=4']
-    start_urls = ['https://www.superjob.ru/vakansii/povar.html?geo%5Bt%5D%5B0%5D=4']
+    # start_urls = ['https://www.superjob.ru/vakansii/testirovshik.html?geo%5Bt%5D%5B0%5D=4']
+    start_urls = ['https://www.superjob.ru/vakansii/testirovschik.html?geo%5Bt%5D%5B0%5D=4']
 
     def parse(self, response: HtmlResponse):
-        # url = 'https://www.superjob.ru'
         links = response.xpath("//a[contains(@class, 'icMQ_ _6AfZ9')]/@href")
         # print()
         for link in links:
@@ -26,15 +26,15 @@ class SjruSpider(scrapy.Spider):
         else:
             min_salary = response.xpath(sj_salary).extract()
             if '—' in min_salary:
-                max_salary = min_salary[4]
-                min_salary = min_salary[0]
+                max_salary = min_salary[4].replace('\xa0', '')
+                min_salary = min_salary[0].replace('\xa0', '')
             elif 'до' in min_salary:
-                max_salary = min_salary[2]
+                max_salary = min_salary[2].replace('\xa0', '').split('руб.')[0]
                 min_salary = None
-            # elif 'от' in min_salary:
             else:
                 max_salary = None
-                min_salary = min_salary[2]
+                min_salary = min_salary[2].replace('\xa0', '').split('руб.')[0]
         link = response.url
+
         print()
         yield JobparserItem(name=name, min_salary=min_salary, max_salary=max_salary, link=link)
